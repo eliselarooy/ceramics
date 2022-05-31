7da2fc7de4ccd20cbc47d776c182d56bf21c58c4
Elise La Rooy <eliselarooy@gmail.com>
2022-05-31 10:05:24 +0100

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/black-stamp.png';
 
 const Header = () => {
   return (
     <>
-      <div>
-        <div className="logo">Logo</div>
+      <div className="header">
+        <img className="logo" src={logo} alt="logo" />
         <h1>ABBIE LA ROOY</h1>
       </div>
       <nav>
